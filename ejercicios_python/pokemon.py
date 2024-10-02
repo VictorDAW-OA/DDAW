0d1163c934d91d1488de98683a8328d4fa8e2e7b
Jillazquez <117284941+Jillazquez@users.noreply.github.com>
2024-10-02 16:15:50 +0200

    fix corrections

--- ejercicios_python/pokemon.py
+++ ejercicios_python/pokemon.py
@@ -4,31 +4,33 @@ url = "https://pokeapi.co/api/v2/type"
 
 def fetch_data(url: str) -> Optional[Dict[str, Any]]:
     # Get the differents types of pokemons
-    response = requests.get(url)
-    if(response.status_code == 200):
-       return response.json()
-    else:
-        print("Error con la peticion, error: "+response.status_code)
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # Throws an exception if there is a problem with the request (4XX - 5XX)
+    except Exception as e:
+        print(f"Error con la excepcion: {e}")
         return None
+    # Return data if the request was succesfull
+    return response.json()
 
 def get_water_type_url(data: Dict[str, any]) -> Optional[str]:
     # Get the url
     for pokemon in data['results']:
         if pokemon['name'] == "water":
             return pokemon['url']
-    return None
+    return None # If cant find the water url return none
 
 
 def fetch_water_pokemons(url: str) -> List[str]:
     # Get the water type pokemons returns none if fails
-    pokeDict = []
-    response = requests.get(url)
+    poke_dict = []
+    response = requests.get(url) # Get a json list with all the water type pokemons
     data = response.json()
     if response.status_code == 200:
-        for pokemon in data['pokemon']:
-            poke = pokemon['pokemon']['name']
-            pokeDict.append(poke)
-        return pokeDict
+        for pokemon in data['pokemon']: 
+            poke = pokemon['pokemon']['name'] # Adds all the pokemons to a dict
+            poke_dict.append(poke)
+        return poke_dict # Returns a dict with all the water type pokemons
     else:
         print("Error a fetch tipo agua"+ data.status_code)
         return None
